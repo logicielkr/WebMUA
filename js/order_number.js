711e4c40259a09730c3a96ce8b474b8e0928b196
logicielkr <graha@graha.kr>
2022-06-24 01:44:36 +0900

    1. ApplyOrderNumberProcessorImpl에서 where 절에 컬럼을 추가할 수 있도록 수정한 것을 반영

--- js/order_number.js
+++ js/order_number.js
@@ -8,23 +8,28 @@ var label = {
 };
 var table = {
 	pk:"graha_mail_account_id",
-	order_column:"order_number"
+	order_column:"order_number",
+	param_names:["parent_id"],
+	param_values:["1"]
 };
 var url = "order_number.xml";
 GrahaCommonOrderNumber.appendButton(url, "graha_mail_account", table, label);
 */
 function GrahaCommonOrderNumber() {
+	
 }
 
 GrahaCommonOrderNumber.appendButton = function(url, tableId, table, label) {
 	if("ontouchstart" in document.documentElement) {
 	} else if($("table#" + tableId + " tbody tr").length > 1) {
-		$("div.nav.bottom div.right").append("<input type='button' value='" + label.default + "' onclick='GrahaCommonOrderNumber.applyOrder(this, \"" + url + "\", \"" + tableId + "\", \"" + table.pk + "\", \"" + table.order_column + "\", \"" + label.default + "\", \"" + label.apply + "\", \"" + label.fail + "\", \"" + label.processing + "\")' />");
+		$("div.nav.bottom div.center").append("<input type='button' value='" + label.default + "' onclick='GrahaCommonOrderNumber.applyOrder(this, \"" + url + "\", \"" + tableId + "\")' />");
+		GrahaCommonOrderNumber.table = table;
+		GrahaCommonOrderNumber.label = label;
 	}
 };
 
-GrahaCommonOrderNumber.applyOrder = function(obj, url, tableId, tablePk, orderColumn, defaultLabel, applyLabel, failLabel, processingLabel) {
-	if($(obj).val() == defaultLabel) {
+GrahaCommonOrderNumber.applyOrder = function(obj, url, tableId) {
+	if($(obj).val() == GrahaCommonOrderNumber.label.default) {
 		$("table#" + tableId + " tbody td").each(function(){
 			$(this).css("width", $(this).width() +"px");
 		});
@@ -33,16 +38,25 @@ GrahaCommonOrderNumber.applyOrder = function(obj, url, tableId, tablePk, orderCo
 			axis: 'y'
 		});
 		$("table#" + tableId + " tbody").disableSelection();
-		$(obj).val(applyLabel);
-	} else if($(obj).val() == applyLabel) {
+		$(obj).val(GrahaCommonOrderNumber.label.apply);
+	} else if($(obj).val() == GrahaCommonOrderNumber.label.apply) {
 		var index = 1;
 		var formData = new FormData();
-		$("table#" + tableId + " tbody td." + tablePk + "").each(function() {
-			formData.append("" + tablePk + "." + index, $(this).text());
-			formData.append("" + orderColumn + "." + index, index);
+		$("table#" + tableId + " tbody td." + GrahaCommonOrderNumber.table.pk + "").each(function() {
+			formData.append("" + GrahaCommonOrderNumber.table.pk + "." + index, $(this).text());
+			formData.append("" + GrahaCommonOrderNumber.table.order_column + "." + index, index);
+			/*
+			console.log("" + GrahaCommonOrderNumber.table.pk + "." + index, $(this).text());
+			console.log("" + GrahaCommonOrderNumber.table.order_column + "." + index, index);
+			*/
 			index++;
 		});
-		$(obj).val(processingLabel);
+		if(GrahaCommonOrderNumber.table.param_names) {
+			for(var i = 0; i < GrahaCommonOrderNumber.table.param_names.length; i++) {
+				formData.append(GrahaCommonOrderNumber.table.param_names[i], GrahaCommonOrderNumber.table.param_values[i]);
+			}
+		}
+		$(obj).val(GrahaCommonOrderNumber.label.processing);
 		$.ajax({
 			url: url,
 			processData: false,
@@ -53,10 +67,10 @@ GrahaCommonOrderNumber.applyOrder = function(obj, url, tableId, tablePk, orderCo
 			success: function(result) {
 				$("table#" + tableId + " tbody").sortable("destroy");
 				$("table#" + tableId + " tbody").enableSelection();
-				$(obj).val(defaultLabel);
+				$(obj).val(GrahaCommonOrderNumber.label.default);
 			},
 			error: function(result) {
-				$(obj).val(failLabel);
+				$(obj).val(GrahaCommonOrderNumber.label.fail);
 			}
 		});
 	}
